15f404390fc7f26343e1ee50847fe035f888fdf6
Rwan Souza <rwan.souza@hotmail.com>
2020-07-23 00:34:31 -0300

    Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,106 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import ProductList from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy/777c62f8aaf845099192aa7600ec841e_9366/Tenis_U_Path_Verde_EE4466_01_standard.jpg"
+          alt=" Tènis"
+        />
+
+        <strong>Tênis</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
